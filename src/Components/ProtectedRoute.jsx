2221410edb73c854871ad7dfff132cdd29535b83
Sugappa Kuruba <kurubasugappa@gmail.com>
2022-07-10 16:25:12 +0530

    Some validations added to the url

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import {useParams,Navigate} from "react-router-dom"
 
 const ProtectedRoute = ({children}) => {
     const {year,month}=useParams()
-    if(year<0||month<1||month>12||typeof year!=="number"||typeof month!=="number")return  <Navigate to="/404"/>   
+    if(year<0||month<1||month>12||typeof year!=="number"||typeof month!=="number"||!month||!year||month===undefined||year===undefined)return  <Navigate to="/404"/>   
 
   return children
 }
